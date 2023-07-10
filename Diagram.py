be73af5eb203fb7486298f1c8b8208662cc3ad2a
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-10 14:33:35 +0700

    Update Diagram.py

--- Diagram.py
+++ Diagram.py
@@ -7,17 +7,18 @@ data = pd.read_json('fire.json')
 
 st.title('Diagram Batang dari File JSON')
 
-    # Memilih kolom untuk ditampilkan
+# Memilih kolom untuk ditampilkan
 column = st.selectbox('Pilih Kolom', data.columns)
 
-    # Menghitung frekuensi nilai pada kolom yang dipilih
+# Menghitung frekuensi nilai pada kolom yang dipilih
 value_counts = data[column].value_counts()
 
-    # Menampilkan diagram batang
-plt.bar(value_counts.index, value_counts.values)
+# Membuat diagram batang
+fig, ax = plt.subplots()
+ax.bar(value_counts.index, value_counts.values)
 plt.xlabel(column)
 plt.ylabel('Frekuensi')
 plt.title('Diagram Batang')
 
-    # Menampilkan diagram batang menggunakan Streamlit
-st.pyplot()
+# Menampilkan diagram batang menggunakan Streamlit
+st.pyplot(fig)
