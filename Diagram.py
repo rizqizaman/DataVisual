f3e67f64bb1576ffe137d351404dca0144e05e95
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-10 13:28:52 +0700

    Update Diagram.py

--- Diagram.py
+++ Diagram.py
@@ -6,11 +6,10 @@ import streamlit as st
 df = pd.read_json('fire.json')
 
 st.title('Visualisasi Data')
-st.subheader('Jumlah Notifikasi Deteksi Api')
 
 plt.figure(figsize=(15, 5))
 sns.countplot(data=df, x='Object_Detection', order=df['Object_Detection'].value_counts().index[:])
-plt.xlabel('Notif Deteksi')
+plt.xlabel('Object Detectioni')
 plt.ylabel('Jumlah')
 plt.xticks(rotation=45)
 
