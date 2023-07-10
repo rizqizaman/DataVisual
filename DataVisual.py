0e2a82e89a3bfc7ba77f5c4a1cea2586afc324ff
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-11 01:00:14 +0700

    Update DataVisual.py

--- DataVisual.py
+++ DataVisual.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import mysql.connector
-# import psycopg2
+import psycopg2
 import pandas as pd
 import matplotlib.pyplot as plt
 import base64
 
 # Fungsi untuk menghubungkan ke database
 def create_connection():
-    connection = mysql.connector.connect(
-    # connection = psycopg2.connect(
+    # connection = mysql.connector.connect(
+    connection = psycopg2.connect(
         host='localhost',
         user='id20864486_bakaranpawon2023',  # Ganti dengan username Anda
         port='3306',
