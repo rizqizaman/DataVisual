d92416bf9231e337a8709839a59ec595f7cedd59
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-11 00:57:19 +0700

    Update DataVisual.py

--- DataVisual.py
+++ DataVisual.py
@@ -16,39 +16,29 @@ def create_connection():
         database='id20864486_bakaran'  # Ganti dengan nama database Anda
     )
 
-conn = create_connection()
-    # cursor = conn.cursor()
-    # Menambahkan Kolom Hari pada database
-query = "SELECT Object_Detection, Location FROM fire ORDER BY Date_Time = NOW()"  # Ganti dengan nama tabel yang ingin Anda ambil datanya
-    # query = "SELECT * FROM fire"  # Ganti dengan nama tabel yang ingin Anda ambil datanya
+def get_data():
+    conn = create_connection()
+    query = "SELECT Object_Detection, Location FROM fire ORDER BY Date_Time = NOW()"  # Ganti dengan nama tabel yang ingin Anda ambil datanya
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
 
-    # cursor.execute(query)
-    # col0 = [column[0] for column in cursor.description]
-    # data = cursor.fetchall()
-df = pd.read_sql_query(query, conn)
-
-    # cursor.close()
-conn.close()
-
-    # return col0, data
-    
 st.title("Visualisasi Data dari Database")
 tabel = get_data()
 column_list = list(tabel.columns)
 selected_column = st.selectbox("Pilih kolom:", column_list)
 
-    # Mengambil data pada kolom terpilih
+# Mengambil data pada kolom terpilih
 data = tabel[selected_column]
 
-    # Menghitung frekuensi nilai pada kolom terpilih
+# Menghitung frekuensi nilai pada kolom terpilih
 value_counts = data.value_counts()
 
-    # Membuat diagram bar menggunakan matplotlib
+# Membuat diagram bar menggunakan matplotlib
 fig, ax = plt.subplots()
 value_counts.plot(kind='bar')
 ax.set_xlabel(selected_column)
 ax.set_ylabel("Jumlah")
 ax.set_title(f"Diagram Bar dari {selected_column}")
 st.pyplot(fig)
-    
-# st.title("Visualisasi Data dari Database")
+plt.show()
