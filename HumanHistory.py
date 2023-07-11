ada8e077b2bd57f8685f0d6b49699e2d3f4a5110
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-11 13:00:14 +0700

    Update HumanHistory.py

--- HumanHistory.py
+++ HumanHistory.py
@@ -13,10 +13,10 @@ def decode_image(encode_string):
     img = Image.open(BytesIO(decode_bytes))
     return img
 st.title("Decoded Images")
-for encode_image in encode_image_list:
-    decoded_image = decode_image(encode_image)
-    st.image(decoded_image, use_column_width=True)
-# for encoded_image, tanggal in zip(encode_image, tanggal):
-#     decoded_image = decode_image(encoded_image[0])
+# for encode_image in encode_image_list:
+#     decoded_image = decode_image(encode_image)
 #     st.image(decoded_image, use_column_width=True)
-#     st.write(tanggal[0])
+for encoded_image, tanggal in zip(encode_image, tanggal):
+    decoded_image = decode_image(encoded_image)
+    st.image(decoded_image, use_column_width=True)
+    st.write(tanggal)
