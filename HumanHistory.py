e013aa9269f9e941021baac38c41ccb5f296f8b1
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-11 12:52:28 +0700

    Update HumanHistory.py

--- HumanHistory.py
+++ HumanHistory.py
@@ -1,69 +1,17 @@
 import streamlit as st
-import mysql.connector
 import base64
 import pandas as pd
 from io import BytesIO
 from PIL import Image
 
-# Fungsi untuk menghubungkan ke database
+data = pd.read_json('fire.json')
+encode_image_list = data['Image']
 
-# Membaca file JSON
-# # def data():
-# data = pd.read_json('fire.json')
-# data
-    # return data
-    
-def create_connection():
-    mysql_config = st.secrets["mysql"]
-    connection = mysql.connector.connect(
-        host=mysql_config['localhost'],
-        user=mysql_config['root'],  # Ganti dengan username Anda
-        password=mysql_config[''],  # Ganti dengan password Anda
-        database=mysql_config['web_service']  # Ganti dengan nama database Anda
-    )
-    return connection
-
-# def get_image_encode():
-#     conn = create_connection()
-#     query = "SELECT Image From fire ORDER BY Object_Detection = 'Person' DESC "
-#     cursor = conn.cursor()
-#     cursor.execute(query)
-#     image_list = cursor.fetchall()
-
-#     conn.close()
-#     return image_list
-
-def Tanggal():
-    conn = create_connection()
-    query = "SELECT Date_Time From fire ORDER BY Object_Detection = 'Person' DESC "
-    cursor = conn.cursor()
-    cursor.execute(query)
-    TGL = cursor.fetchall()
-
-    conn.close()
-    return TGL
-
-
-# def decode_image(encode_string):
-#     decode_bytes = base64.b64decode(encode_string)
-#     img = Image.open(BytesIO(decode_bytes))
-#     return img
-    
-# def main():
-#     st.title("Human History")
-#     encode_image = get_image_encode()
-#     tanggal = Tanggal()
-#     for encoded_image, tanggal in zip(encode_image, tanggal):
-#         decoded_image = decode_image(encoded_image[0])
-#         st.image(decoded_image, use_column_width=True)
-#         st.write(tanggal[0])
-
-st.title("Human History")
-# data = data()
-# for encoded_image, tanggal in zip(encode_image, tanggal):
-#     decoded_image = decode_image(encoded_image[0])
-#     st.image(decoded_image, use_column_width=True)
-#     st.write(tanggal[0])
-    # st.image(decoded_image, caption=tanggal, use_column_width=True)
-tgl = Tanggal()
-tgl
+def decode_image(encode_string):
+    decode_bytes = base64.b64decode(encode_string)
+    img = Image.open(BytesIO(decode_bytes))
+    return img
+st.title("Decoded Images")
+for encode_image in encode_image_list:
+    decoded_image = decode_image(encode_image)
+    st.image(decoded_image, use_column_width=True)
