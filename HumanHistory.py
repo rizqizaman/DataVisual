b3c602e1fc75537597609333def78f0e580a18d0
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-11 14:12:34 +0700

    Update HumanHistory.py

--- HumanHistory.py
+++ HumanHistory.py
@@ -6,7 +6,8 @@ from PIL import Image
 
 data = pd.read_json('fire.json')
 encode_image_list = data['Image']
-tanggal = data['Date_Time']
+tanggal = tanggal
+# data['Date_Time']
 
 def decode_image(encode_string):
     decode_bytes = base64.b64decode(encode_string)
