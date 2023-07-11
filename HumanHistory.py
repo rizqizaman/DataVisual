7ed6bb2ff1d33b0f02ef415128b014b68b2c2538
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-11 13:30:04 +0700

    Update HumanHistory.py

--- HumanHistory.py
+++ HumanHistory.py
@@ -1,22 +1,59 @@
+# import streamlit as st
+# import base64
+# import pandas as pd
+# from io import BytesIO
+# from PIL import Image
+
+# data = pd.read_json('fire.json')
+# encode_image_list = data['Image']
+# tanggal = data['Date_Time']
+
+# def decode_image(encode_string):
+#     decode_bytes = base64.b64decode(encode_string)
+#     img = Image.open(BytesIO(decode_bytes))
+#     return img
+# st.title("Decoded Images")
+# for encoded_image, tanggal in zip(encode_image_list, tanggal):
+#     decoded_image = decode_image(encoded_image)
+#     st.image(decoded_image, use_column_width=True)
+#     st.write(tanggal)
+
+
 import streamlit as st
 import base64
 import pandas as pd
 from io import BytesIO
 from PIL import Image
+from datetime import datetime
 
-data = pd.read_json('fire.json')
+data = pd.read_json('nama_file.json')
+data['Date_Time'] = pd.to_datetime(data['Date_Time'])
+data = data.sort_values('Date_Time', ascending=False)
 encode_image_list = data['Image']
-tanggal = data['Date_Time']
+
 
 def decode_image(encode_string):
     decode_bytes = base64.b64decode(encode_string)
     img = Image.open(BytesIO(decode_bytes))
     return img
-st.title("Decoded Images")
-# for encode_image in encode_image_list:
-#     decoded_image = decode_image(encode_image)
+    
+st.title("Data Berdasarkan Tanggal Terupdate")
+# for index, row in data.iterrows():
+#     st.write("Tanggal:", row['Date_Time'])
+#     st.image(row['Image'])
+
+    
+# st.title("Decoded Images")
+# for encoded_image, tanggal in zip(encode_image_list, tanggal):
+#     decoded_image = decode_image(encoded_image)
 #     st.image(decoded_image, use_column_width=True)
-for encoded_image, tanggal in zip(encode_image_list, tanggal):
+#     st.write(tanggal)
+
+ for _, row in data_sorted.iterrows():
+    encoded_image = row['Image']
+    tanggal = row['Date_Time']
     decoded_image = decode_image(encoded_image)
-    st.image(decoded_image, use_column_width=True)
-    st.write(tanggal)
+
+        # Menampilkan gambar dan tanggal
+    st.image(decoded_image, caption=str(tanggal), use_column_width=True)
+
