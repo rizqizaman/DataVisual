c0959fc7b3e68afadef24bc5794d54031a6cb857
Zaman <70796021+rizqizaman@users.noreply.github.com>
2023-07-11 14:21:50 +0700

    Update LiveStream.py

--- LiveStream.py
+++ LiveStream.py
@@ -1,16 +1,20 @@
-# import streamlit as st
-# import cv2
-# from PIL import Image
-    
-# st.title('Live Stream menggunakan Streamlit')
+import streamlit as st
+from PIL import Image
+import numpy as np
 
-#     # Membuka kamera
-# cap = cv2.VideoCapture("http://192.168.71.137:8080/video")
+img_file_buffer = st.camera_input("Take a picture")
 
-#     # Looping untuk menampilkan livestream
-# while True:
-#   ret, frame = cap.read()
-#   frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+if img_file_buffer is not None:
+    # To read image file buffer as a PIL Image:
+    img = Image.open(img_file_buffer)
 
-#         # Menampilkan livestream pada halaman web Streamlit
-#   st.image(frame_rgb, channels='RGB')
+    # To convert PIL Image to numpy array:
+    img_array = np.array(img)
+
+    # Check the type of img_array:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(img_array))
+
+    # Check the shape of img_array:
+    # Should output shape: (height, width, channels)
+    st.write(img_array.shape)
